5e86a8a7d2145b35c1848b1466e53a4eb16527df
AlphesterX1 <ajay.kausik@gmail.com>
2026-01-24 17:26:42 +0530

    Artemis First Commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Artemis from './Artemis.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Artemis />
+  </React.StrictMode>,
+)
